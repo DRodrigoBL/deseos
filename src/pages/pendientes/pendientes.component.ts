24e43f5fa070f0ff215d6e2d5e049b5845d5a9a7
rodbautista360 <rodrigo.bautista@sec360.com.mx>
2017-05-26 11:17:09 -0500

    creacion de los componentes de pendientes y terminados

---
+++ src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'pendientes',
+  templateUrl: 'pendientes.component.html'
+})
+
+export class PendientesComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
